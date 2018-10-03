80caad241b2af461efd3c986d3a1f4b6d173384d
ldeoliveira <leocc010@gmail.com>
2018-10-03 18:32:25 -0300

    Create create_table.sql

---
+++ database/create_table.sql
@@ -0,0 +1,5 @@
+create database if not exists companies;
+
+use companies;
+
+create table if not exists company (company_name varchar(255), stock_name varchar(255), market_value varchar(255), oscillation varchar(255));
